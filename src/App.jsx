823a59c04db73c3ec018066114655484c878ef65
Manzar <manzarinemanzar@gmail.com>
2024-08-10 22:21:16 +0545

    URL error is showing which needs to be fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import './App.css'
 import authService from "./appwrite/auth"
-import { login, logout } from './store/authSlice'
+import {login, logout} from "./store/authSlice"
 import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
 
@@ -13,22 +13,21 @@ function App() {
   useEffect(() => {
     authService.getCurrentUser()
     .then((userData) => {
-      if(userData) {
+      if (userData) {
         dispatch(login({userData}))
       } else {
         dispatch(logout())
       }
     })
-    .catch ((error) => console.log(error))
     .finally(() => setLoading(false))
   }, [])
-
+  
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
-      <div className="w-full block">
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
         <Header />
         <main>
-          <Outlet />
+        TODO:  <Outlet />
         </main>
         <Footer />
       </div>
