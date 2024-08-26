487511b960ee4756f200f2037149670deae9467f
Manzar <manzarinemanzar@gmail.com>
2024-08-26 14:10:59 +0545

    Styles corrected

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,10 @@ function App() {
   
   return !loading ? (
     
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'
+    style={{
+      margin: '0 -2.2rem'
+     }}>
       <div className='w-full block'>
         <Header />
         <main>
